ddf70911fb9721b9f9b8b495c431afcc5d690f6b
Asamac <takumi091111@gmail.com>
2018-06-12 17:46:13 +0900

    fix bug

--- index.js
+++ index.js
@@ -4,61 +4,84 @@ const client = new Discord.Client()
 
 let editMessage;
 let interval;
+let timer;
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
+
   // 更新間隔を設定
-  interval = 20 * 60 * 1000
+  interval = 5 * 1000
 })
 
 client.on('message', async msg => {
+
   if (msg.content === '!server') {
-    const message = [];
-    const servers = await scraping()
-
-    for (server of servers) {
-      message.push(`${server.info} | ${server.name} | ${server.address}`)
-    }
-
-    msg.channel.send(message.join('\n'), { code: true })
+    const message = await getServerList()
+    msg.channel.send(message, { code: true })
   }
 
   if (msg.content === '!loop') {
-    const timer = setInterval(async () => {
-      const message = [];
-      const servers = await scraping()
-  
-      for (server of servers) {
-        message.push(`${server.info} | ${server.name} | ${server.address}`)
-      }
 
-      // 合計サーバー数
-
-      message.unshift(`合計サーバー数: ${servers.length}`)
-  
-      // 更新日時
-
-      const now = new Date()
-      const time = [
-        now.toLocaleDateString('ja-jp'),
-        now.toLocaleTimeString('ja-jp')
-      ].join(' / ')
-      message.unshift(`更新日時: ${time}`)
-
-      // 合計参加人数
-      
-      // -> 面倒臭いからパス
+    timer = setInterval(async () => {
+      const message = await getServerList()
 
       // 更新処理
-
       if (editMessage != undefined) {
-        editMessage.edit(message.join('\n'), { code: true })
+        editMessage.edit(message, { code: true })
       } else {
-        editMessage = await msg.channel.send(message.join('\n'), { code: true })
+        editMessage = await msg.channel.send(message, { code: true })
       }
+
+      // 更新間隔を変更
+      interval = 20 * 60 * 1000
+      clearInterval(timer)
+
+      // 再登録
+      timer = setInterval(async () => {
+        const message = await getServerList()
+
+        if (editMessage != undefined) {
+          editMessage.edit(message, { code: true })
+        } else {
+          editMessage = await msg.channel.send(message, { code: true })
+        }
+      }, interval)
 
     }, interval)
   }
+
+  // 更新処理の停止
+  if (msg.content === '!stop') {
+    if (timer != undefined) {
+      clearInterval(timer)
+      interval = 5 * 1000
+    }
+  }
 });
+
+const getServerList = async () => {
+  const message = [];
+  const servers = await scraping()
+
+  for (server of servers) {
+    message.push(`${server.info} | ${server.name} | ${server.address}`)
+  }
+
+  // 合計サーバー数
+  message.unshift(`合計サーバー数: ${servers.length}`)
+
+  // 更新日時
+  const now = new Date()
+  const time = [
+    now.toLocaleDateString('ja-jp'),
+    now.toLocaleTimeString('ja-jp')
+  ].join(' / ')
+  message.unshift(`更新日時: ${time}`)
+
+  // 合計参加人数
+  // -> 面倒臭いからパス
+
+  return message.join('\n')
+}
 
 client.login('Token')
